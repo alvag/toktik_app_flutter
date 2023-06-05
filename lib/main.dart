873cfe88e76c56b0c68ae606672dc9bad3dd355b
Max Alva Garcia <alva85@gmail.com>
2023-06-05 15:02:20 -0500

    Datasource y Repository

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:toktik/config/theme/app_theme.dart';
+import 'package:toktik/infraestructure/datasources/local_video_datasource_impl.dart';
+import 'package:toktik/infraestructure/repositories/video_post_repository_impl.dart';
 import 'package:toktik/presentation/providers/discover_provider.dart';
 import 'package:toktik/presentation/screens/discover/discover_screen.dart';
 
@@ -14,18 +16,24 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final videoPostRepository = VideoPostsRepositoryImpl(
+      videosDatasource: LocalVideoDatasource(),
+    );
+
     return MultiProvider(
       providers: [
         ChangeNotifierProvider<DiscoverProvider>(
           // lazy: false,
-          create: (_) => DiscoverProvider()..loadNextPage(),
+          create: (_) =>
+              DiscoverProvider(videoPostRepository: videoPostRepository)
+                ..loadNextPage(),
         ),
       ],
       child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Toktik',
-        theme: AppTheme().getTheme(),
-        home: const DiscoverScreen()
+          debugShowCheckedModeBanner: false,
+          title: 'Toktik',
+          theme: AppTheme().getTheme(),
+          home: const DiscoverScreen()
       ),
     );
   }
