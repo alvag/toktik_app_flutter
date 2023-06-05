4ddae410b4b8bf3a0fb1ca145270ae5928f1f916
Max Alva Garcia <alva85@gmail.com>
2023-06-05 08:35:57 -0500

    Video player - final

--- lib/presentation/widgets/video/fullscreen_player.dart
+++ lib/presentation/widgets/video/fullscreen_player.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:toktik/presentation/widgets/video/video_background.dart';
 import 'package:video_player/video_player.dart';
 
 class FullScreenPlayer extends StatefulWidget {
@@ -37,18 +38,56 @@ class _FullScreenPlayerState extends State<FullScreenPlayer> {
       future: controller.initialize(),
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.done) {
-          return AspectRatio(
-            aspectRatio: controller.value.aspectRatio,
-            child: VideoPlayer(controller),
-          );
-        } else {
-          return const Center(
-            child: CircularProgressIndicator(
-              strokeWidth: 2,
+          return GestureDetector(
+            onTap: () {
+              if (controller.value.isPlaying) {
+                controller.pause();
+              } else {
+                controller.play();
+              }
+            },
+            child: AspectRatio(
+              aspectRatio: controller.value.aspectRatio,
+              child: Stack(children: [
+                VideoPlayer(controller),
+                VideoBackground(stops: const [0.8, 1.0]),
+                Positioned(
+                  bottom: 50,
+                  left: 20,
+                  child: _VideoCaption(caption: widget.caption),
+                ),
+              ]),
             ),
           );
         }
+
+        return const Center(
+          child: CircularProgressIndicator(
+            strokeWidth: 2,
+          ),
+        );
       },
+    );
+  }
+}
+
+class _VideoCaption extends StatelessWidget {
+  final String caption;
+
+  const _VideoCaption({required this.caption});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    final titleStyle = Theme.of(context).textTheme.titleLarge;
+
+    return SizedBox(
+      width: size.width * 0.6,
+      child: Text(
+        caption,
+        maxLines: 2,
+        style: titleStyle,
+      ),
     );
   }
 }
