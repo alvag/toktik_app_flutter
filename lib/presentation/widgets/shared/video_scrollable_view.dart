4230dc4c9e6399e59b3592bc0c7ce52576d17baf
Max Alva Garcia <alva85@gmail.com>
2023-06-05 08:02:18 -0500

    Video player

--- lib/presentation/widgets/shared/video_scrollable_view.dart
+++ lib/presentation/widgets/shared/video_scrollable_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:toktik/domain/entities/video_post.dart';
 import 'package:toktik/presentation/widgets/shared/video_buttons.dart';
+import 'package:toktik/presentation/widgets/video/fullscreen_player.dart';
+
 class VideoScrollableView extends StatelessWidget {
   final List<VideoPost> videos;
 
@@ -17,8 +19,11 @@ class VideoScrollableView extends StatelessWidget {
 
         return Stack(
           children: [
-            // VideoPlayer(videoPost: videoPost),
-
+            SizedBox.expand(
+                child: FullScreenPlayer(
+              videoUrl: videoPost.videoUrl,
+              caption: videoPost.caption,
+            )),
             Positioned(
               bottom: 40,
               right: 20,
